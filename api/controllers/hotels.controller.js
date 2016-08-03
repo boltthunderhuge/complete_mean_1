9397a5da264381bbc649e89560ee126cedab4311
Graeme Anderson <graemeanderson@kabaminc.com>
2016-08-03 14:54:54 -0700

    mongoose-ified the GET routes

--- api/controllers/hotels.controller.js
+++ api/controllers/hotels.controller.js
@@ -1,32 +1,33 @@
-var dbConnection = require('../data/dbconnection.js');
-var ObjectId = require('mongodb').ObjectId;
-var hotelData = require('../data/hotel-data.json');
+var mongoose = require('mongoose');
+var Hotel = mongoose.model('Hotel');
+
 
 module.exports.hotelsGetAll = function(req, res) {
-
-	var db = dbConnection.get();
-	var collection = db.collection('hotels');
-
 	var offset = (req.query && req.query.offset) ? parseInt(req.query.offset) : 0;
-	var count = (req.query && req.query.count) ? parseInt(req.query.count) : docs.length - 1;
+	var count = (req.query && req.query.count) ? parseInt(req.query.count) : 1;
 
-	collection.find().skip(offset).limit(count).toArray(function(err, docs) {
-		res.status(200).json(docs);
+	Hotel.find().skip(offset).limit(count).exec(function(err, hotels) {
+		console.log('found ', hotels.length);
+		res.json(hotels);
 	});
+	/*collection.find().skip(offset).limit(count).toArray(function(err, docs) {
+		res.status(200).json(docs);
+	});*/
 };
 
 module.exports.hotelsGetOne = function(req, res) {
+	var hotelId = req.params.hotelId;
 
-	var db = dbConnection.get();
-	var collection = db.collection('hotels');
+	Hotel.findById(hotelId).exec(function(err, hotel) {
+		console.log('found hotel');
+		res.json(hotel);
+	});
 
-	var hotelId = ObjectId(req.params.hotelId);
-
-	collection.findOne({
+	/*collection.findOne({
 		_id: hotelId
 	}, function(err, doc) {
 		res.status(200).json(doc);
-	});
+	});*/
 };
 
 
